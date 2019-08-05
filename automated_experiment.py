3b4ebe07f0e9fc7c0f012cee37186a0ccab11a07
fr293 <fr293@cam.ac.uk>
2019-08-05 16:59:34 +0100

    Initial commit

---
+++ automated_experiment.py
@@ -0,0 +1,53 @@
+import numpy as np
+import brightfield_threaded as bt
+import power_supply_current_controller_threaded as pscct
+from pyfiglet import Figlet
+import time
+import threading
+from Tkinter import *
+import tkFileDialog
+
+# read in data from input csv, and program experiments
+
+f = Figlet(font='isometric1')
+print f.renderText('Ferg')
+time.sleep(1)
+g = Figlet(font='isometric3')
+print g.renderText('Labs')
+time.sleep(1)
+
+print('Welcome to the automated brightfield creep test tool.')
+time.sleep(0.5)
+
+print('please select the file that contains details of the experiments to be run:')
+
+root = Tk()
+root.withdraw()
+root.attributes("-topmost", True)
+root.filename = tkFileDialog.askopenfilename(initialdir='C:/', title='Select file',
+                                             filetypes=(('csv files', '*.csv'), ('all files', '*.*')))
+print('success: you have selected ' + root.filename)
+
+file_array = np.genfromtxt(root.filename, dtype=None, skip_header=1, delimiter=',', encoding=None)
+file_list = file_array.tolist()
+
+print('please select the folder that you want the raw data to be saved in:')
+
+root.directory = tkFileDialog.askdirectory()
+output_folder = root.directory + '/'
+print('success: you have selected ' + output_folder)
+
+print('success: starting experiment')
+
+for experiment_run in file_list:
+    # extract current configurations
+    [filename, ca, cc, fon, fdur, num_frames, frame_period, temp] = experiment_run
+    c_thread = threading.Thread(name='m_thread', target=bt.multiframe,
+                                args=(num_frames, frame_period, output_folder, filename))
+    m_thread = threading.Thread(name='c_thread', target=pscct.time_currents, args=(cc, ca, fon, fdur))
+    print('performing experiment: ' + filename)
+    c_thread.start()
+    m_thread.start()
+    c_thread.join()
+    m_thread.join()
+
---
+++ power_supply_current_controller_threaded.py
@@ -0,0 +1,31 @@
+# A command-line interface for the TTi power supply based current controller
+
+import time
+import power_supply_current_controller as pscc
+import threading
+
+
+def trigger_currents(connection_object, duration):
+    pscc.switch_on(connection_object)
+    time.sleep(duration)
+    pscc.switch_off(connection_object)
+
+def setup_timer(time_on):
+    time.sleep(time_on)
+
+
+def time_currents(config, amplitude, time_on, duration):
+    setup_timer_thread = threading.Thread(name='setup_timer_thread', target=setup_timer, args=[time_on])
+    setup_timer_thread.start()
+    connection_object = pscc.open_controller()
+    time.sleep(1)
+    pscc.write_values(connection_object, config, amplitude)
+    time.sleep(0.02)
+    current_controller = threading.Thread(name='current_controller', target=trigger_currents,
+                                          args=(connection_object, duration))
+    pscc.light_on(connection_object)
+    setup_timer_thread.join()
+    current_controller.start()
+    current_controller.join()
+    time.sleep(0.02)
+    pscc.close_controller(connection_object)
